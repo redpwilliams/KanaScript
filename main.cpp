9dcb4a6f39261f937367978cae3823e3b6e1c7b1
Red Williams <redpwilliams@gmail.com>
2024-05-17 20:22:33 -0400

    move parsing code to lexer.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstring>
-#include <cstdint>
 #include "Lexer.hpp"
-#include <io.h>
-#include <fcntl.h>
 #include "lib/utf8/checked.h"
 
 int main(int argc, char* argv[])
@@ -22,8 +18,6 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-//    _setmode(_fileno(stdout), _O_U16TEXT);
-
     std::string line;
     while (getline(sourceFile, line)) {
        Lexer::tokenize(line);
@@ -31,28 +25,9 @@ int main(int argc, char* argv[])
     sourceFile.close();
 
     std::string ex{"書く"};
-    Token t(Token::TokenType::KEYWORD, ex.substr(0, 2));
+    Token t(Token::TokenType::KEYWORD, line.substr(0, 2));
     printToken(t);
 
-    std::cout << "\nIs valid? " << utf8::is_valid(ex) << std::endl;
-
-    char* str = (char*)ex.c_str();    // utf-8 string
-    char* str_i = str;                  // string iterator
-    char* end = str+strlen(str)+1;      // end iterator
-
-    do
-    {
-        uint32_t code = utf8::next(str_i, end); // get 32 bit code of an utf-8 symbol
-        if (code == 0)
-            continue;
-
-        unsigned char symbol[5] = {0};
-        utf8::append(code, symbol); // copy code to symbol
-
-        std::cout << symbol << std::endl;
-        // ... do something with symbol
-    }
-    while ( str_i < end );
 
 }
 
