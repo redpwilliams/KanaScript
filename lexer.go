4a2c2dc00e48e483ed35f76ab65fad31c59c6df7
Red Williams <redpwilliams@gmail.com>
2024-05-18 04:17:00 -0400

    refactor: new NextTokenIs function

--- lexer.go
+++ lexer.go
@@ -10,20 +10,30 @@ type Lexer struct {
 
 func (l *Lexer) Tokenize(input string) {
 	//Read and interpret each character
-	for i := 0; i < len([]rune(input)); i++ {
+	characters := []rune(input)
+	for i := 0; i < len(characters); i++ {
 		// Write
-		if input[i:i+len("書く")] == "書く" {
+		//if input[i:i+len("書く")] == "書く" {
+		if NextTokenIs("書く", input, i) {
 			l.tokens = append(l.tokens, Token{"書く", Kaku})
 			i += len([]rune("書く"))
 		}
+		//else if input[i:i+len("「")] == "「" {
+		//	i++
+		//	closed := false
+		//	for i < len(input) && !closed {
+		//		if input[i:i+len("」")] == "」" {
+		//			closed = true
+		//			l.tokens = append(l.tokens, Token{"test", String})
+		//		}
+		//		i++
+		//	}
+		//}
 
 		fmt.Printf("%c\n", []rune(input)[i])
 	}
-	//for _, char := range []rune(input) {
-	//	fmt.Printf("%c", char)
-	//	fmt.Println(string(char) == "書")
-	//}
-	//strTokens := strings.Fields(input)
-	//l.tokens = append(l.tokens, strTokens...)
 
+}
+func NextTokenIs(token, input string, pos int) bool {
+	return input[pos:pos+len(token)] == token
 }
